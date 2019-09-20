af5fe848ca4f29fcd11c89d9b024e5edcd166109
Angela Weber <angelaweber95@hotmail.com>
2019-09-19 20:21:21 -0700

    add function for ruby.Also, try to figure out why python and ruby functions are not working correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-
-
 function python(animalInput, seasonInput, holidayInput, heroInput, getAwayInput){
   var threeOfFive = 0;
   var pythonWins = false;
@@ -25,25 +23,58 @@ function python(animalInput, seasonInput, holidayInput, heroInput, getAwayInput)
   return pythonWins;
 };
 
+function ruby(animalInput, seasonInput, holidayInput, heroInput, getAwayInput){
+  var threeOfFive = 0;
+  var rubyWins = false;
+
+  if (animalInput === "2") {
+    threeOfFive ++;
+  }
+  if (seasonInput === "2") {
+    threeOfFive ++;
+  }
+  if (holidayInput === "2") {
+    threeOfFive ++;
+  }
+  if (heroInput === "2") {
+    threeOfFive ++;
+  }
+  if (getAwayInput === "2") {
+    threeOfFive ++;
+  }
+  if (threeOfFive >= 3){
+    rubyWins === true
+  }
+  if (rubyWins === true){
+    $('.ruby').show();
+  }
+
+};
+
+
 
 $(document).ready(function() {
   $('form#programming-suggester').submit(function(event) {
+    event.preventDefault();
+
     var nameInput = $('input#name').val();
     var animalInput = $("input:radio[name=animal]:checked").val();
     var seasonInput = $("input:radio[name=season]:checked").val();
     var holidayInput = $("input:radio[name=holiday]:checked").val();
     var heroInput = $("input:radio[name=hero]:checked").val();
     var getAwayInput = $("input:radio[name=get-away]:checked").val();
-    // console.log(animalInput);
-    // console.log(seasonInput);
-    // console.log(holidayInput);
-    // console.log(heroInput);
-    // console.log(getAwayInput);
-    if (python(animalInput, seasonInput, holidayInput, heroInput, getAwayInput) === true) {
-      $('.python').show();
-    }
+    console.log(animalInput);
+    console.log(seasonInput);
+    console.log(holidayInput);
+    console.log(heroInput);
+    console.log(getAwayInput);
+    $('.ruby').show();
+
+
     $('.name').text(nameInput);
-    event.preventDefault();
+
     alert("does this make it?")
+
+
   });
 });
